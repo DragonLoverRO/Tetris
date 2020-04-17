c7d7958a4a89f333dbc13b7379ac57a99027cceb
alkadmi <59713614+alkadmi@users.noreply.github.com>
2020-04-16 21:10:24 -0400

    a commit

--- UWP_Tetris/MainPage.xaml.cs
+++ UWP_Tetris/MainPage.xaml.cs
@@ -12,9 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Microsoft.Graphics.Canvas.UI;
+using Windows.UI;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
+//sources: https://github.com/EricCharnesky/CIS297-Winter2020/tree/master/PongExample/PongExample
 namespace UWP_Tetris
 {
     /// <summary>
@@ -27,17 +30,19 @@ namespace UWP_Tetris
             this.InitializeComponent();
         }
 
-        private void Canvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        private void Canvas_CreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
         {
-
+            
         }
 
-        private void Canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args)
+        private void Canvas_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
         {
-
+            args.DrawingSession.DrawRectangle(10, 10, 10, 700, Colors.Red);
+            args.DrawingSession.DrawRectangle(790, 10, 10, 700, Colors.Red);
+            args.DrawingSession.DrawRectangle(10, 700, 790, 10, Colors.Red);
         }
 
-        private void Canvas_Update(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedUpdateEventArgs args)
+        private void Canvas_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
         {
 
         }
